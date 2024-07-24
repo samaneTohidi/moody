38aa53f96dedc62a4be3aae7e95d452e0b9aab27
samane Tohidi <s.tohidi39@gmail.com>
2024-07-24 20:10:01 +0330

    Fix some bugs in Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moody/screens/seek_bar_screen.dart';
+import 'package:firebase_core/firebase_core.dart' show Firebase;
+import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,10 +19,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-
       ),
       debugShowCheckedModeBanner: false,
 
