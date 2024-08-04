d9c7d5dc6dcc7d6dcf74734dd4ab2ffafa7ad0ea
samane Tohidi <s.tohidi39@gmail.com>
2024-08-04 15:21:27 +0330

    Add .gitignore.save to .gitignore and remove from cache

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:moody/screens/auth_gate.dart';
 import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'firebase_options.dart';
 
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load(fileName: "assets/.env");
+    print("Environment variables loaded successfully.");
+  } catch (e) {
+    print("Error loading environment variables: $e");
+  }
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Request permissions
+  await FirebaseMessaging.instance.requestPermission();
+
+  // Initialize local notifications
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  // Create notification channel
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description: 'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
   runApp(const MyApp());
 }
 
@@ -28,4 +69,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,17 @@
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:moody/screens/search_user_screen.dart';
-
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart';
 import '../models/emotional_status_update_model.dart';
-import '../models/notification_model.dart';
 
-
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 class HomeScreen extends StatefulWidget {
   final User user;
@@ -25,13 +29,13 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   final List<String> _emotions = ['😡', '😟', '😕', '😐', '🙂', '😊', '😄'];
 
   final List<Color> _backgroundColors = [
-    Colors.red,
-    Colors.orange,
-    Colors.yellow,
+    Colors.red.shade700,
+    Colors.orange.shade700,
+    Colors.yellow.shade700,
     Colors.yellowAccent,
-    Colors.lightGreen,
-    Colors.green,
-    Colors.greenAccent,
+    Colors.lightGreen.shade700,
+    Colors.green.shade700,
+    Colors.greenAccent.shade400,
   ];
 
   bool _isLoading = true;
@@ -41,74 +45,191 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
     _loadUserEmotionalStatus();
+    _initializeNotifications();
+    _saveFCMToken();
+    FirebaseMessaging.onMessage.listen(_handleMessage);
+  }
+
+  Future<void> _initializeNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      description: 'This channel is used for important notifications.', // description
+      importance: Importance.high,
+    );
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+  }
+
+  Future<void> _saveFCMToken() async {
+    try {
+      final token = await FirebaseMessaging.instance.getToken();
+      if (token != null) {
+        final userDoc = FirebaseFirestore.instance.collection('users').doc(widget.user.uid);
+        await userDoc.update({'fcmToken': token});
+      }
+    } catch (e) {
+      print('Error saving FCM token: $e');
+    }
   }
 
   Future<void> _loadUserEmotionalStatus() async {
-    final userDoc =
-    FirebaseFirestore.instance.collection('users').doc(widget.user.uid);
-    final docSnapshot = await userDoc.get();
-    if (docSnapshot.exists) {
-      final emotionalStatus = docSnapshot.data()?['emotionalStatus'] ?? '🙂';
-      final index = _emotions.indexOf(emotionalStatus);
-      if (index != -1) {
-        setState(() {
-          _currentValue = index.toDouble();
-        });
+    try {
+      final userDoc = FirebaseFirestore.instance.collection('users').doc(widget.user.uid);
+      final docSnapshot = await userDoc.get();
+      if (docSnapshot.exists) {
+        final emotionalStatus = docSnapshot.data()?['emotionalStatus'] ?? '🙂';
+        final index = _emotions.indexOf(emotionalStatus);
+        if (index != -1) {
+          setState(() {
+            _currentValue = index.toDouble();
+          });
+        }
       }
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (e) {
+      print('Error loading user emotional status: $e');
+      setState(() {
+        _isLoading = false;
+      });
     }
-    setState(() {
-      _isLoading = false;
-    });
   }
 
   Future<void> _updateUserProfile(String emotion) async {
-    final userDoc =
-    FirebaseFirestore.instance.collection('users').doc(widget.user.uid);
-    await userDoc.update({
-      'emotionalStatus': emotion,
-      'updatedAt': Timestamp.now(),
-    });
+    try {
+      final userDoc = FirebaseFirestore.instance.collection('users').doc(widget.user.uid);
+      await userDoc.update({'emotionalStatus': emotion, 'updatedAt': Timestamp.now()});
 
-    final statusUpdateDoc = FirebaseFirestore.instance.collection('EmotionalStatusUpdates').doc();
-    final statusUpdate = EmotionalStatusUpdateModel(
-      updateId: statusUpdateDoc.id,
-      userId: widget.user.uid,
-      emotionalStatus: emotion,
-      createdAt: Timestamp.now(),
-    );
+      final statusUpdateDoc = FirebaseFirestore.instance.collection('EmotionalStatusUpdates').doc();
+      final statusUpdate = EmotionalStatusUpdateModel(
+        updateId: statusUpdateDoc.id,
+        userId: widget.user.uid,
+        emotionalStatus: emotion,
+        createdAt: Timestamp.now(),
+      );
 
-    await statusUpdateDoc.set(statusUpdate.toJson());
-
-    _notifyFollowers(widget.user.uid, emotion);
+      await statusUpdateDoc.set(statusUpdate.toJson());
+      _notifyFollowers(widget.user.uid, emotion);
+    } catch (e) {
+      print('Error updating user profile: $e');
+    }
   }
 
   Future<void> _notifyFollowers(String userId, String emotion) async {
-    final followersQuery = await FirebaseFirestore.instance
-        .collection('followers')
-        .where('user_id', isEqualTo: userId)
-        .get();
+    try {
+      final userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+      final userEmail = userDoc.data()?['email'] ?? 'Unknown';
 
-    for (var doc in followersQuery.docs) {
-      final followerId = doc['follower_user_id'];
-      final notificationDoc = FirebaseFirestore.instance.collection('notifications').doc();
-      final notification = NotificationModel(
-        notificationId: notificationDoc.id,
-        userId: followerId,
-        fromUserId: userId,
-        notificationType: 'status_update',
-        createdAt: Timestamp.now(),
+      final followersQuery = await FirebaseFirestore.instance.collection('followers').where('user_id', isEqualTo: userId).get();
+
+      for (var doc in followersQuery.docs) {
+        final followerId = doc['follower_user_id'];
+        final followerDoc = await FirebaseFirestore.instance.collection('users').doc(followerId).get();
+        final token = followerDoc.data()?['fcmToken'];
+
+        if (token != null) {
+          await _sendFCMNotification(token, userEmail, emotion);
+        }
+      }
+    } catch (e) {
+      print('Error notifying followers: $e');
+    }
+  }
+
+  Future<void> _sendFCMNotification(String token, String userEmail, String emotion) async {
+
+    try {
+      final String projectId = dotenv.env['PROJECT_ID'] ?? 'Not Found';
+      final String privateKeyId = dotenv.env['PRIVATE_KEY_ID'] ?? 'Not Found';
+      final String privateKey = dotenv.env['PRIVATE_KEY'] ?? 'Not Found';
+      final String clientEmail = dotenv.env['CLIENT_EMAIL'] ?? 'Not Found';
+      final String clientId = dotenv.env['CLIENT_ID'] ?? 'Not Found';
+      final String authUri = dotenv.env['AUTH_URI'] ?? 'Not Found';
+      final String tokenUri = dotenv.env['TOKEN_URI'] ?? 'Not Found';
+      final String authProviderCertUrl = dotenv.env['AUTH_PROVIDER_CERT_URL'] ?? 'Not Found';
+      final String clientCertUrl = dotenv.env['CLIENT_CERT_URL'] ?? 'Not Found';
+      final String serviceAccountKeyString = '''
+    {
+      "type": "service_account",
+      "project_id": "$projectId",
+      "private_key_id": "$privateKeyId",
+      "private_key": "$privateKey",
+      "client_email": "$clientEmail",
+      "client_id": "$clientId",
+      "auth_uri": "$authUri",
+      "token_uri": "$tokenUri",
+      "auth_provider_x509_cert_url": "$authProviderCertUrl",
+      "client_x509_cert_url": "$clientCertUrl",
+      "universe_domain": "googleapis.com"
+    } ''';
+
+      if (serviceAccountKeyString == 'Not Found') {
+        throw Exception('Service Account Key not found in environment variables');
+      }
+
+      final credentials = ServiceAccountCredentials.fromJson(serviceAccountKeyString);
+      final client = await clientViaServiceAccount(credentials, ['https://www.googleapis.com/auth/firebase.messaging']);
+      final accessToken = (await client.credentials).accessToken.data;
+
+      final fcmEndpoint = 'https://fcm.googleapis.com/v1/projects/$projectId/messages:send';
+
+      final message = {
+        'message': {
+          'token': token,
+          'notification': {
+            'title': 'Emotional Status Update',
+            'body': 'Your friend $userEmail updated their status to $emotion',
+          },
+        }
+      };
+
+      final headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      };
+
+      final response = await http.post(
+        Uri.parse(fcmEndpoint),
+        headers: headers,
+        body: jsonEncode(message),
       );
-      await notificationDoc.set(notification.toJson());
 
+      if (response.statusCode == 200) {
+        print('FCM request for device sent!');
+      } else {
+        print('FCM request for device failed with status code ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error sending FCM notification: $e');
+    }
+  }
 
-      // Send notification via FCM (requires FCM setup and integration)
-      // await FirebaseMessaging.instance.sendMessage(
-      //   to: followerId,
-      //   data: {
-      //     'title': 'Emotional Status Update',
-      //     'body': 'User $userId has updated their status to $emotion',
-      //   },
-      // );
+  void _handleMessage(RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+
+    if (notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'high_importance_channel', // id
+            'High Importance Notifications', // title
+            channelDescription: 'This channel is used for important notifications.', // description
+            icon: android.smallIcon,
+          ),
+        ),
+      );
     }
   }
 
@@ -220,8 +341,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                           inactiveTrackColor: Colors.white,
                           overlayColor: Colors.yellow.withOpacity(0.2),
                           thumbShape: LightningThumbShape(),
-                          overlayShape: const RoundSliderOverlayShape(
-                              overlayRadius: 20.0),
+                          overlayShape: const RoundSliderOverlayShape(overlayRadius: 20.0),
                         ),
                         child: Slider(
                           value: _currentValue,
@@ -263,19 +383,19 @@ class LightningThumbShape extends SliderComponentShape {
 
   @override
   void paint(
-    PaintingContext context,
-    Offset center, {
-    required Animation<double> activationAnimation,
-    required Animation<double> enableAnimation,
-    required bool isDiscrete,
-    required TextPainter labelPainter,
-    required RenderBox parentBox,
-    required SliderThemeData sliderTheme,
-    required TextDirection textDirection,
-    required double value,
-    required double textScaleFactor,
-    required Size sizeWithOverflow,
-  }) {
+      PaintingContext context,
+      Offset center, {
+        required Animation<double> activationAnimation,
+        required Animation<double> enableAnimation,
+        required bool isDiscrete,
+        required TextPainter labelPainter,
+        required RenderBox parentBox,
+        required SliderThemeData sliderTheme,
+        required TextDirection textDirection,
+        required double value,
+        required double textScaleFactor,
+        required Size sizeWithOverflow,
+      }) {
     final Canvas canvas = context.canvas;
 
     final Paint fillPaint = Paint()
@@ -351,6 +471,7 @@ class FollowersList extends StatelessWidget {
     );
   }
 }
+
 class FollowingList extends StatelessWidget {
   final String userId;
 
